e64e660b30bb0671fbc8f36aceafd90d89d29f16
blarrise <61951559+blariise@users.noreply.github.com>
2025-05-03 21:41:41 +0200

    Add populating display with buttons clicks

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 let operator;
 let left_number;
 let right_number;
+let display_text = "";
 
 function add(a, b) {
   return a + b;
@@ -30,3 +31,25 @@ function operate(op, a, b) {
       return divide(a, b);
   }
 }
+
+function shouldItDisplay(button) {
+  console.log(display_text.length);
+  return display_text.length < 9 
+         && button.getAttribute("class") === "number";
+}
+
+function calculator() {
+  const buttons = document.querySelectorAll("button");
+  const display = document.querySelector(".text");
+  buttons.forEach((button) => {
+    let is_number = button.getAttribute("class") === "number";
+    button.addEventListener("click", () => {
+      if (shouldItDisplay(button)) {
+        display_text += button.textContent;
+        display.textContent = display_text;
+      }
+    });
+  });
+}
+
+calculator();
